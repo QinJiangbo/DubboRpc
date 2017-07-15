7116ead15ddbc513cc9910a3b2d417fc17e60d6a
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-07-16 01:40:08 +0800

    start service provider

--- RpcServiceConsumer/src/main/java/com/qinjiangbo/dubbo/RpcConsumer.java
+++ RpcServiceConsumer/src/main/java/com/qinjiangbo/dubbo/RpcConsumer.java
@@ -1,9 +1,27 @@
 package com.qinjiangbo.dubbo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * @date: 16/07/2017 12:44 AM
  * @author: qinjiangbo@github.io
  */
 public class RpcConsumer {
 
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("conf/dubbo-service-consumer.xml");
+        context.start();
+
+        LoginService loginService = (LoginService) context.getBean("loginService");
+        System.out.println("Richard existed? " + loginService.exist("Richard"));
+
+        try {
+            TimeUnit.SECONDS.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
