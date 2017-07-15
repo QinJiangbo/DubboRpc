41652c2c5c486c911cf20b20a1136accec046d66
QinJiangbo <QinJiangbo@users.noreply.github.com>
2017-07-16 01:06:12 +0800

    start service provider

--- RpcServiceProvider/src/main/java/com/qinjiangbo/dubbo/RpcProvider.java
+++ RpcServiceProvider/src/main/java/com/qinjiangbo/dubbo/RpcProvider.java
@@ -1,8 +1,23 @@
 package com.qinjiangbo.dubbo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * @date: 16/07/2017 12:45 AM
  * @author: qinjiangbo@github.io
  */
 public class RpcProvider {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("config/dubbo-service-provider.xml");
+        context.start();
+        try {
+            TimeUnit.SECONDS.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
